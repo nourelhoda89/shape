56c819dcacc1a5e23d4b2288854c2d4ad928a818
نور الهدى <nfarhat6558@volmail.jalc.edu>
2017-02-28 11:43:43 -0600

    fix

--- TwoDimensionalShape.java
+++ TwoDimensionalShape.java
@@ -1,20 +1,5 @@
 package shape;
 abstract public class TwoDimensionalShape {
- /*private double area;
-   private double perimeter;
-   
-  private TwoDimensionalShape(){
-      this.area=0;
-      this.perimeter=0;
-   }
-   TwoDimensionalShape(double area,double perimeter){
-      this.area=area;
-      this.perimeter=perimeter;
-   }
-   public String toString(){
-      return "area is " +this.area +", perimeter is " + this.perimeter;
-   }*/
    abstract public double area();
    abstract public double perimeter();
-  
 }
