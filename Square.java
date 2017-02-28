e3f440069d3980413816d2ae8b4dde9682ef5b7a
نور الهدى <nfarhat6558@volmail.jalc.edu>
2017-02-28 11:46:14 -0600

    worked on square

--- Square.java
+++ Square.java
@@ -1,4 +1,22 @@
 package shape;
 
-public class Square extends TwoDimensionalShape{
+ public class Square extends Rectangle{
+   private double side;
+   
+   private Square(){
+      super(0.0,0.0);
+      this.side=0;
+   }
+   public Square(double side){
+       super(side,side);
+   }
+    public String toString(){
+      return "The area of the square is " +getArea() +"\nThe peremeter of the square is " +getPerimeter();
+   }
+   //just to check will delete later
+   public static void main(String[] args){
+      Square square = new Square(5);
+      System.out.println(square.toString()); 
+   }
+   
 }
